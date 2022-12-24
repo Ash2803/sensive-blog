a9c1a1b0f497d380f0dba3bb742faf09c5c4616d
Ash2803 <ashot.93@mail.ru>
2022-12-24 18:15:14 +0400

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,7 @@
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 from django.shortcuts import render
 
 from blog.models import Post, Tag
-
-
-def get_related_posts_count(tag):
-    return tag.posts.count()
 
 
 def serialize_post(post):
@@ -17,7 +13,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in Tag.objects.popular()[:5]],
         'first_tag_title': post.tags.first().title,
     }
 
